269ec04776fbae6ca9242175fd518c5c1ae2accc
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-11-03 04:47:00 +0200

    +

---
+++ packages/common/src/config.ts
@@ -0,0 +1,15 @@
+
+/**
+ * @ignore
+ */
+export class Config {
+  private static instance: Config;
+
+  public static getInstance(): Config {
+    if (!Config.instance) {
+      Config.instance = new Config();
+    }
+    return Config.instance;
+  }
+
+}
